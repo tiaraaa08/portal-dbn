07037839b8e99029ea5e3820c97fe0199b732931
shikhon islam <sikhonhapue333@gmail.com>
2024-10-17 20:17:43 +0600

    project-check

---
+++ assets/js/main.js
@@ -0,0 +1,531 @@
+/*-----------------------------------------------------------------
+
+Template Name: RevAuto - Car Dealer & Services Html Template
+Author:  Gramentheme
+Author URI: https://themeforest.net/user/gramentheme/portfolio
+Version: 1.0.0
+Description: RevAuto - Car Dealer & Services Html Template<
+
+-------------------------------------------------------------------
+CSS TABLE OF CONTENTS
+-------------------------------------------------------------------
+
+01. header
+02. animated text with swiper slider
+03. magnificPopup
+04. counter up
+05. wow animation
+06. nice select
+07. swiper slider
+08. search popup
+09. mousecursor 
+09. preloader 
+
+
+------------------------------------------------------------------*/
+
+(function($) {
+    "use strict";
+
+    $(document).ready( function() {
+
+        //>> Mobile Menu Js Start <<//
+        $('#mobile-menu').meanmenu({
+            meanMenuContainer: '.mobile-menu',
+            meanScreenWidth: "1199",
+            meanExpand: ['<i class="far fa-plus"></i>'],
+        });
+
+        //>> Sidebar Toggle Js Start <<//
+        $(".offcanvas__close,.offcanvas__overlay").on("click", function() {
+            $(".offcanvas__info").removeClass("info-open");
+            $(".offcanvas__overlay").removeClass("overlay-open");
+        });
+        $(".sidebar__toggle").on("click", function() {
+            $(".offcanvas__info").addClass("info-open");
+            $(".offcanvas__overlay").addClass("overlay-open");
+        });
+
+        //>> Body Overlay Js Start <<//
+        $(".body-overlay").on("click", function() {
+            $(".offcanvas__area").removeClass("offcanvas-opened");
+            $(".df-search-area").removeClass("opened");;
+            $(".body-overlay").removeClass("opened");
+        });
+
+        //>> Sticky Header Js Start <<//
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 250) {
+                $("#header-sticky").addClass("sticky");
+            } else {
+                $("#header-sticky").removeClass("sticky");
+            }
+        });
+
+        //>> Video Popup Start <<//
+        $(".img-popup").magnificPopup({
+            type: "image",
+            gallery: {
+                enabled: true,
+            },
+        });
+
+        $('.video-popup').magnificPopup({
+            type: 'iframe',
+            callbacks: {
+            }
+        });
+        
+        //>> Counterup Start <<//
+        $(".count").counterUp({
+            delay: 15,
+            time: 4000,
+        });
+
+        //>> Wow Animation Start <<//
+        new WOW().init();
+
+        //>> Nice Select Start <<//
+        $('select').niceSelect();
+
+        const heroSlider = new Swiper('.hero-slider', {
+            speed: 2500,
+            loop: true,
+            //slidesPerView: 1,
+            autoplay: true,
+            effect: 'fade',
+            // breakpoints: {
+            //     '1600': {
+            //         slidesPerView: 1,
+            //     },
+            //     '1400': {
+            //         slidesPerView: 1,
+            //     },
+            //     '1200': {
+            //         slidesPerView: 1,
+            //     },
+            //     '992': {
+            //         slidesPerView: 1,
+            //     },
+            //     '768': {
+            //         slidesPerView: 1,
+            //     },
+            //     '576': {
+            //         slidesPerView: 1,
+            //     },
+            //     '0': {
+            //         slidesPerView: 1,
+            //     },
+    
+                a11y: false,
+            // },
+            pagination: {
+                el: ".dot",
+                clickable: true,
+            },
+    
+        });
+
+        //>> Testimonial-slider Slider Start <<//
+        if($('.testimonial-slider').length > 0) {
+            const testimonialSlider = new Swiper(".testimonial-slider", {
+                spaceBetween: 30,
+                speed: 2000,
+                loop: true,
+                autoplay: {
+                    delay: 1000,
+                    disableOnInteraction: false,
+                },
+                pagination: {
+                    el: ".dots",
+                    clickable: true,
+                },
+
+                breakpoints: {
+                    991: {
+                        slidesPerView: 2,
+                    },
+                    767: {
+                        slidesPerView: 1,
+                    },
+                    575: {
+                        slidesPerView: 1,
+                    },
+                    0: {
+                        slidesPerView: 1,
+                    },
+                },
+            });
+        }
+
+        if($('.testimonial-slider-2').length > 0) {
+            const testimonialSlider2 = new Swiper(".testimonial-slider-2", {
+                spaceBetween: 30,
+                speed: 2000,
+                loop: true,
+                autoplay: {
+                    delay: 1000,
+                    disableOnInteraction: false,
+                },
+                breakpoints: {
+                    1399: {
+                        slidesPerView: 4,
+                    },
+                    1199: {
+                        slidesPerView: 3,
+                    },
+                    991: {
+                        slidesPerView: 2,
+                    },
+                    767: {
+                        slidesPerView: 2,
+                    },
+                    575: {
+                        slidesPerView: 1,
+                    },
+                    0: {
+                        slidesPerView: 1,
+                    },
+                },
+            });
+        }
+
+        if($('.testimonial-slider-3').length > 0) {
+            const testimonialSlider3 = new Swiper(".testimonial-slider-3", {
+                spaceBetween: 30,
+                speed: 2000,
+                loop: true,
+                autoplay: {
+                    delay: 1000,
+                    disableOnInteraction: false,
+                },
+                breakpoints: {
+                    1399: {
+                        slidesPerView: 5,
+                    },
+                    1199: {
+                        slidesPerView: 3,
+                    },
+                    991: {
+                        slidesPerView: 2,
+                    },
+                    767: {
+                        slidesPerView: 2,
+                    },
+                    575: {
+                        slidesPerView: 1,
+                    },
+                    0: {
+                        slidesPerView: 1,
+                    },
+                },
+            });
+        }
+
+        //>> Testimonial-slider Slider Start <<//
+        if($('.team-slider').length > 0) {
+            const teamSlider = new Swiper(".team-slider", {
+                spaceBetween: 30,
+                speed: 2000,
+                loop: true,
+                autoplay: {
+                    delay: 1000,
+                    disableOnInteraction: false,
+                },
+                pagination: {
+                    el: ".dots",
+                    clickable: true,
+                },
+
+                breakpoints: {
+                    991: {
+                        slidesPerView: 2,
+                    },
+                    767: {
+                        slidesPerView: 1,
+                    },
+                    575: {
+                        slidesPerView: 1,
+                    },
+                    0: {
+                        slidesPerView: 1,
+                    },
+                },
+            });
+        }
+
+        //>> Brand Slider Start <<//
+        if($('.brand-slider').length > 0) {
+            const brandSlider = new Swiper(".brand-slider", {
+                spaceBetween: 30,
+                speed: 2000,
+                loop: true,
+                autoplay: {
+                    delay: 2000,
+                    disableOnInteraction: false,
+                },
+
+                breakpoints: {
+                    1399: {
+                        slidesPerView: 6,
+                    },
+                    1199: {
+                        slidesPerView: 5,
+                    },
+                    991: {
+                        slidesPerView: 4,
+                    },
+                    767: {
+                        slidesPerView: 3,
+                    },
+                    575: {
+                        slidesPerView: 2,
+                    },
+                    0: {
+                        slidesPerView: 1,
+                    },
+                },
+            });
+        }
+        
+
+        //>> Shop-slider Slider Start <<//
+        if($('.shop-slider').length > 0) {
+            const shopSlider = new Swiper(".shop-slider", {
+                spaceBetween: 24,
+                speed: 2000,
+                loop: true,
+                autoplay: {
+                    delay: 1000,
+                    disableOnInteraction: false,
+                },
+                pagination: {
+                    el: ".dot",
+                    clickable: true,
+                },
+                navigation: {
+                    nextEl: ".array-prev",
+                    prevEl: ".array-next",
+                },
+                breakpoints: {
+                    1399: {
+                        slidesPerView: 6,
+                    },
+                    1199: {
+                        slidesPerView: 5,
+                    },
+                    991: {
+                        slidesPerView: 4,
+                    },
+                    767: {
+                        slidesPerView: 3,
+                    },
+                    575: {
+                        slidesPerView: 2,
+                    },
+                    0: {
+                        slidesPerView: 1,
+                    },
+                },
+            });
+        }
+
+        //>> Quantity Cart Js Start <<//
+        let quantity = 0;
+        let price = 0;
+        $(".cart-item-quantity-amount, .product-quant").html(quantity);
+        $(".total-price, .product-pri").html(price.toFixed(2));
+        $(".cart-increment, .cart-incre").on("click", function() {
+            if (quantity <= 4) {
+                quantity++;
+                $(".cart-item-quantity-amount, .product-quant").html(quantity);
+                let basePrice = $(".base-price, .base-pri").text();
+                $(".total-price, .product-pri").html((basePrice * quantity).toFixed(2));
+            }
+        });
+
+        $(".cart-decrement, .cart-decre").on("click", function() {
+            if (quantity >= 1) {
+                quantity--;
+                $(".cart-item-quantity-amount, .product-quant").html(quantity);
+                let basePrice = $(".base-price, .base-pri").text();
+                $(".total-price, .product-pri").html((basePrice * quantity).toFixed(2));
+            }
+        });
+
+        $(".cart-item-remove>a").on("click", function() {
+            $(this).closest(".cart-item").hide(300);
+        });
+
+        //>> Quantity Js Start <<//
+    $(".quantity").on("click", ".plus", function (e) {
+        let $input = $(this).prev("input.qty");
+        let val = parseInt($input.val(), 10); // Specify base 10
+        $input.val(val + 1).change();
+    });
+
+    $(".quantity").on("click", ".minus", function (e) {
+        let $input = $(this).next("input.qty");
+        let val = parseInt($input.val(), 10); // Specify base 10
+        if (val > 0) {
+            $input.val(val - 1).change();
+        }
+    });
+
+         // 11.Range sliger
+         function getVals() {
+            let parent = this.parentNode;
+            let slides = parent.getElementsByTagName("input");
+            let slide1 = parseFloat(slides[0].value);
+            let slide2 = parseFloat(slides[1].value);
+            if (slide1 > slide2) {
+                let tmp = slide2;
+                slide2 = slide1;
+                slide1 = tmp;
+            }
+
+            let displayElement = parent.getElementsByClassName("rangeValues")[0];
+            displayElement.innerHTML = "$" + slide1 + " - $" + slide2;
+        }
+
+        window.onload = function () {
+            let sliderSections = document.getElementsByClassName("range-slider");
+            for (let x = 0; x < sliderSections.length; x++) {
+                let sliders = sliderSections[x].getElementsByTagName("input");
+                for (let y = 0; y < sliders.length; y++) {
+                    if (sliders[y].type === "range") {
+                        sliders[y].oninput = getVals;
+                        sliders[y].oninput();
+                    }
+                }
+            }
+        }
+
+        progressBar: () => {
+            const pline = document.querySelectorAll(".progressbar.line");
+            const pcircle = document.querySelectorAll(".progressbar.semi-circle");
+            pline.forEach(e => {
+                const line = new ProgressBar.Line(e, {
+                    strokeWidth: 6,
+                    trailWidth: 6,
+                    duration: 3000,
+                    easing: 'easeInOut',
+                    text: {
+                        style: {
+                            color: 'inherit',
+                            position: 'absolute',
+                            right: '0',
+                            top: '-30px',
+                            padding: 0,
+                            margin: 0,
+                            transform: null
+                        },
+                        autoStyleContainer: false
+                    },
+                    step: (state, line) => {
+                        line.setText(Math.round(line.value() * 100) + ' %');
+                    }
+                });
+                let value = e.getAttribute('data-value') / 100;
+                new Waypoint({
+                    element: e,
+                    handler: function () {
+                        line.animate(value);
+                    },
+                    offset: 'bottom-in-view',
+                })
+            });
+            pcircle.forEach(e => {
+                const circle = new ProgressBar.SemiCircle(e, {
+                    strokeWidth: 6,
+                    trailWidth: 6,
+                    duration: 2000,
+                    easing: 'easeInOut',
+                    step: (state, circle) => {
+                        circle.setText(Math.round(circle.value() * 100));
+                    }
+                });
+                let value = e.getAttribute('data-value') / 100;
+                new Waypoint({
+                    element: e,
+                    handler: function () {
+                        circle.animate(value);
+                    },
+                    offset: 'bottom-in-view',
+                })
+            });
+        }
+
+        const rangeInput = document.querySelectorAll(".range-input input"),
+            priceInput = document.querySelectorAll(".price-input input"),
+            range = document.querySelector(".slider .progress");
+        let priceGap = 1000;
+
+        priceInput.forEach((input) => {
+            input.addEventListener("input", (e) => {
+                let minPrice = parseInt(priceInput[0].value),
+                    maxPrice = parseInt(priceInput[1].value);
+
+                if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+                    if (e.target.className === "input-min") {
+                        rangeInput[0].value = minPrice;
+                        range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+                    } else {
+                        rangeInput[1].value = maxPrice;
+                        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+                    }
+                }
+            });
+        });
+
+        rangeInput.forEach((input) => {
+            input.addEventListener("input", (e) => {
+                let minVal = parseInt(rangeInput[0].value),
+                    maxVal = parseInt(rangeInput[1].value);
+
+                if (maxVal - minVal < priceGap) {
+                    if (e.target.className === "range-min") {
+                        rangeInput[0].value = maxVal - priceGap;
+                    } else {
+                        rangeInput[1].value = minVal + priceGap;
+                    }
+                } else {
+                    priceInput[0].value = minVal;
+                    priceInput[1].value = maxVal;
+                    range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+                    range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+                }
+            });
+        });
+       
+        //>> Back To Top Start <<//
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 20) {
+                $("#back-top").addClass("show");
+            } else {
+                $("#back-top").removeClass("show");
+            }
+        });
+        $("#back-top").click(function () {
+            $("html, body").animate({ scrollTop: 0 }, 800);
+            return false;
+        });
+	// Back to top btn area end here ***
+
+
+    }); // End Document Ready Function
+
+    function loader() {
+        $(window).on('load', function() {
+            // Animate loader off screen
+            $(".preloader").addClass('loaded');                    
+            $(".preloader").delay(600).fadeOut();                       
+        });
+    }
+
+    loader();
+   
+
+})(jQuery); // End jQuery
+
